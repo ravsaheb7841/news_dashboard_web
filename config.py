ba694dfa25320db17e2cad1954db9daa16451970
ravsaheb7841 <bansoderav@gmail.com.com>
2025-04-10 15:43:08 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 
 # News API Key
 NEWS_API_KEY = "your_api_key_here"  # Replace with your actual News API key
-# Ensure to keep this key secure and do not expose it in public repositories.
-
+# Ensure to keep this key secure and do not expose it in public repositories
