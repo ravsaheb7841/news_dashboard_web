3414faf248a0fc7b5b29d159408a773748454cf5
ravsaheb7841 <bansoderav@gmail.com.com>
2025-04-10 17:30:47 +0530

    Final edi

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 # News API Key
-NEWS_API_KEY = "your_api_key_here"  # Replace with your actual News API key
-# Ensure to keep this key secure and do not expose it in public repositories
+NEWS_API_KEY = "220cf0fc01744c609a262a7409b31aea"  # Replace with your actual News API key
+
